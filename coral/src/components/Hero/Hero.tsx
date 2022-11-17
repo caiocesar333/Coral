f60b41d31789f9acb6d5661afda159a8ea36b6ad
Caio <caiocesar333@outlook.com>
2022-11-17 16:13:31 -0300

    Refatorando o Hero para comportar um botao

--- coral/src/components/Hero/Hero.tsx
+++ coral/src/components/Hero/Hero.tsx
@@ -1,10 +1,21 @@
-import { Container, Img } from "./style";
+import { Button, Carry, Container, H1, H3, Img, P, Wrapper } from "./style";
 import HeroImg from "../../assets/hero.png"
+import Arrow from "../../assets/arrow.svg"
 
-export function Hero(){
-    return(
+export function Hero() {
+    return (
         <Container>
             <Img src={HeroImg}></Img>
+            <Wrapper>
+                <Carry>
+                    <H1>Carry your Funk</H1>
+                    <H3>Trendy handbags collection for your party animal</H3>
+                    <Button>
+                        <img src={Arrow} alt="" />
+                        <P>See more</P>
+                    </Button>
+                </Carry>
+            </Wrapper>
         </Container>
     )
 }
--- coral/src/components/Hero/style.ts
+++ coral/src/components/Hero/style.ts
@@ -9,5 +9,73 @@ export const Container = styled.div`
 `;
 
 export const Img = styled.img`
+    border-radius: 24px;
+`
+export const Wrapper = styled.div`
+    position: absolute;
+    width: 77.5%;
+    display: flex;
+    justify-content: end;
 `
 
+export const Carry = styled.div`
+    width: 600px;
+    height: 316px;
+    background: rgba(222, 222, 222, 0.7);
+    backdrop-filter: blur(2.5px);
+
+    border-top-left-radius:24px ;
+    border-bottom-left-radius:24px ;
+
+    display: flex;
+    flex-direction: column;
+    
+`
+
+export const H1 = styled.h1`
+    width: 500px;
+    height: 30px;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 55px;
+    line-height: 84px;
+    color: #1B4B66;
+    margin-left: 40px;
+    `
+
+export const H3 = styled.h3`
+    width: 500px;
+    height: 50px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 28px;
+    line-height: 38px;
+    color: #1B4B66;
+    margin-left: 40px;   
+    margin-top: 15px;
+    `
+
+export const Button = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;    
+    background: #1B4B66;
+    width: 130px ;
+    height: 20px;
+    border-radius: 8px;
+    padding: 10px 20px;
+    color: white;
+    border: none;
+    outline: none;
+    margin-left: 40px;
+    margin-top: 15px;
+    `
+
+export const P = styled.p`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 15px;
+    margin-left: 5px;
+`
