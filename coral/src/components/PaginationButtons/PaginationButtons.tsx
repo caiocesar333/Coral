dbe4e81fd332c70acb9e38d7c86f61ca50667e10
Caio <caiocesar333@outlook.com>
2022-12-05 03:02:07 -0300

    Debugando a paginação

--- coral/src/components/PaginationButtons/PaginationButtons.tsx
+++ coral/src/components/PaginationButtons/PaginationButtons.tsx
@@ -4,19 +4,29 @@ import { Container, Next, Pages, Wrapper } from "./style";
 
 export function PaginationButtons() {
 
-    const redirect = ()=>{
-        window.location.href = ""
+    const redirect = (page:number)=>{
+        window.location.href = `http://localhost:3001/handbags/${page}`
     }
 
 
     return (
         <Container>
             <Wrapper>
-                <Pages>1</Pages>
-                <Pages>2</Pages>
-                <Pages>3</Pages>
-                <Pages>4</Pages>
-                <Pages>5</Pages>
+                <Pages onClick={()=>{
+                    redirect(1)
+                }}>1</Pages>
+                <Pages onClick={()=>{
+                    redirect(2)
+                }}>2</Pages>
+                <Pages onClick={()=>{
+                    redirect(3)
+                }}>3</Pages>
+                <Pages onClick={()=>{
+                    redirect(4)
+                }}>4</Pages>
+                <Pages onClick={()=>{
+                    redirect(5)
+                }}>5</Pages>
             </Wrapper>
 
             <Next>Next</Next>
