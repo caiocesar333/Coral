e9168d56ef9ce6419f3939632939f581e931668d
Caio <caiocesar333@outlook.com>
2022-12-05 03:29:52 -0300

    Adicionando o filtro de pesquisas

--- coral/src/components/PaginationButtons/PaginationButtons.tsx
+++ coral/src/components/PaginationButtons/PaginationButtons.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Container, Next, Pages, Wrapper } from "./style";
 
 
--- coral/src/components/SearchBar/SearchBar.tsx
+++ coral/src/components/SearchBar/SearchBar.tsx
@@ -1,19 +1,39 @@
 import { InputDiv, Img, Input } from "./style";
 import Search from "../../assets/search.svg"
-
+import { useState } from "react";
+import "../../styles/global.css"
 
 export interface SearchBarProps {
     children?: string,
     placeholder: string
 }
 
+const redirect = (item: string) => {
+    if(item==="handbags" || item==="Handbags"){
+        window.location.href = `/${item}/1`
+    }else{
+        let str = item.charAt(0).toUpperCase() + item.slice(1)
+        window.location.href = `/product/${str}/${str}Image`
+    }
+}
+
 
 export function SearchBar({ placeholder }: SearchBarProps) {
+
+    const [item, setItem] = useState("")
+
+    const handle = (e: any) => {
+        e.preventDefault()
+        redirect(item)
+    }
     return (
         <>
             <InputDiv >
                 <Img src={Search} alt="search" />
-                <Input placeholder={placeholder}/>
+                <form onSubmit={handle}>
+                    <Input onChange={event => setItem(event.target.value)} placeholder={placeholder} />
+                    <button className="buttonSubmit">Submit</button>
+                </form>
             </InputDiv>
         </>
     )
